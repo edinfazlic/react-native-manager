71cbcd55e373836c97d05efb25e9c61ea2f72166
Edin Fazlic <edin.fazlic@serapion-it.com>
2018-03-16 20:15:28 +0100

    Added: Authentication

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
-export default class App extends React.Component {
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBOPur6ZDhGykTXS3rt-D3-6_ivp-x4Hnk',
+      authDomain: 'react-native-manager-5958f.firebaseapp.com',
+      databaseURL: 'https://react-native-manager-5958f.firebaseio.com',
+      projectId: 'react-native-manager-5958f',
+      storageBucket: 'react-native-manager-5958f.appspot.com',
+      messagingSenderId: '569403961063'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
